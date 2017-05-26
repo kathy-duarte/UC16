0646f595278436bea8da779d8a14a24ca8479214
Caterin A Duarte <cd02048@srcs.loc>
2017-05-26 09:16:09 -0700

    phone functions

--- ajaxfunctions.js
+++ ajaxfunctions.js
@@ -1,19 +1,45 @@
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+function changeElementClass(id, className) {
+    var el = document.getElementById(id);
+    el.className = className;
+}
+function checkNum(val) {
+    if (Number.parseInt(val) || val == "0") {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-function validate() {
-  $("#result").text("");
-  var email = $("#email").val();
-  if (validateEmail(email)) {
-    $("#result").text(email + " is valid");
-    $("#result").css("color", "black");
-  } else {
-    $("#result").text(email + " is not valid :(");
-    $("#result").css("color", "red");
-  }
-  return false;
+function validPhone(phoneNum) {
+    var count = phoneNum.length
+    if (count == 13 && phoneNum.charAt(0) == '(' && checkNum(phoneNum.charAt(1)) && checkNum(phoneNum.charAt(2)) && checkNum(phoneNum.charAt(3)) && phoneNum.charAt(4) == ')' && checkNum(phoneNum.charAt(5)) && checkNum(phoneNum.charAt(6)) && checkNum(phoneNum.charAt(7)) && phoneNum.charAt(8) == '-' && checkNum(phoneNum.charAt(9)) && checkNum(phoneNum.charAt(10)) && checkNum(phoneNum.charAt(11)) && checkNum(phoneNum.charAt(12))) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-$("#validate").bind("click", validate);
+function CheckPhoneNum(inputId, outputId) {
+    var input = document.getElementById(inputId).value;
+    var outputText = "";
+    if (validPhone(input) == true) {
+        outputText = " The number " + input + " is a valid phone number.";
+        setValidStyle(outputId);
+    }
+    else if (validPhone(input) == false) {
+        outputText = "The number " + input + " is not a valid phone number.";
+        setInvalidStyle(outputId);
+    }
+    document.getElementById(outputId).innerHTML = outputText;
+}
+function setInvalidStyle(id) {
+    var el = document.getElementById(id);
+    el.style.color = "black";
+    el.style.fontSize = "25px";
+    el.style.borderStyle = "solid";
+    el.style.borderColor = "black";
+    el.style.borderRadius = "10px";
+    el.style.padding = "10px";
+}
